13e4f2b5c01fddbea182395445f9bd311cca8b5f
Ddanilo9 <interiordanilo3@gmail.com>
2022-07-29 17:16:17 +0200

    start bonus

--- js/app.js
+++ js/app.js
@@ -1,19 +1,38 @@
 // console.log('hola')
 
 let dimensioneGriglia = 10    //dichiaro il numero delle colonne della griglia
-let numeroCelle = dimensioneGriglia **2     //dichiaro il numero di celle
+let numeroCelle = dimensioneGriglia ** 2    //dichiaro il numero di celle
 let griglia = document.querySelector('.griglia')  //recupero la tabella ove appenderò la griglia
 let btnPlay = document.querySelector('.btn-play')  //recupero il bottone per iniziare a giocare 
+let selectedValue
       //quando clicco parte il gioco
+
+// generaGriglia(dimensioneGriglia, griglia)
+// function generaGriglia(dimensione, griglia){
+// let numeroCelle = dimensione ** 2
+// }
+
+
     btnPlay.addEventListener('click', function(){
+        if(document.getElementById('lv1').checked) {   
+            selectedValue === ('lv1').value;  
         for (let i = 0; i < numeroCelle; i++) {
             let cell = getSquareElement()
             cell.innerHTML = i +1
             griglia.append(cell);
-        }
+        }} else if(document.getElementById('lv2').checked) {   
+            selectedValue === ('lv2').value;
+            griglia.classList.remove('griglia')
+            griglia.classList.add('griglia2')  
+            dimensioneGriglia = 9
+            numeroCelle = dimensioneGriglia ** 2
+        for (let i = 0; i < numeroCelle; i++) {
+            let cell = getSquareElement()
+            cell.innerHTML = i +1
+            griglia.append(cell);
+        }}
+         
         
-    
-
         function getSquareElement () {
         const square = document.createElement('div');
         square.classList.add('cella');
@@ -22,7 +41,6 @@ let btnPlay = document.querySelector('.btn-play')  //recupero il bottone per ini
         return square
         
         }
-
 
         function clickCell() {
             const square = this
