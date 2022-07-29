574b452f97dc6a71ca2e9e7a3dd15461b0205132
Ddanilo9 <interiordanilo3@gmail.com>
2022-07-29 18:05:52 +0200

    add select options (non ancora funzionanti)

--- js/app.js
+++ js/app.js
@@ -1,36 +1,29 @@
 // console.log('hola')
 
 let dimensioneGriglia = 10    //dichiaro il numero delle colonne della griglia
-let numeroCelle = dimensioneGriglia ** 2    //dichiaro il numero di celle
-let griglia = document.querySelector('.griglia')  //recupero la tabella ove appenderò la griglia
-let btnPlay = document.querySelector('.btn-play')  //recupero il bottone per iniziare a giocare 
-let selectedValue
-      //quando clicco parte il gioco
+let numeroCelle = dimensioneGriglia **2   //dichiaro il numero di celle
+let griglia = document.querySelector('.griglia') 
 
-// generaGriglia(dimensioneGriglia, griglia)
-// function generaGriglia(dimensione, griglia){
-// let numeroCelle = dimensione ** 2
+let btnPlay = document.querySelector('.btn-play')  //recupero il bottone per iniziare a giocare 
+let selectedValue = document.getElementById('livelli')
+
+
+let livelloDifficolta = selectedValue.value;
+console.log(livelloDifficolta)
+
+// if (livelloDifficolta === 2) {
+//     dimensioneGriglia = 9;
+// } else if (livelloDifficolta === 3) {
+//     dimensioneGriglia = 7;
 // }
 
-
     btnPlay.addEventListener('click', function(){
-        if(document.getElementById('lv1').checked) {   
-            selectedValue === ('lv1').value;  
+        
         for (let i = 0; i < numeroCelle; i++) {
             let cell = getSquareElement()
             cell.innerHTML = i +1
             griglia.append(cell);
-        }} else if(document.getElementById('lv2').checked) {   
-            selectedValue === ('lv2').value;
-            griglia.classList.remove('griglia')
-            griglia.classList.add('griglia2')  
-            dimensioneGriglia = 9
-            numeroCelle = dimensioneGriglia ** 2
-        for (let i = 0; i < numeroCelle; i++) {
-            let cell = getSquareElement()
-            cell.innerHTML = i +1
-            griglia.append(cell);
-        }}
+        } 
          
         
         function getSquareElement () {
