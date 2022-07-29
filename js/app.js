650c337ecfd4769d9bea548029ef849e9f0f2c12
Ddanilo9 <interiordanilo3@gmail.com>
2022-07-29 15:27:08 +0200

    add eventListener

--- js/app.js
+++ js/app.js
@@ -1,28 +1,36 @@
-console.log('hola')
+// console.log('hola')
 
 let dimensioneGriglia = 10    //dichiaro il numero delle colonne della griglia
 let numeroCelle = dimensioneGriglia **2     //dichiaro il numero di celle
 let griglia = document.querySelector('.griglia')  //recupero la tabella ove appenderò la griglia
 let btnPlay = document.querySelector('.btn-play')  //recupero il bottone per iniziare a giocare 
       //quando clicco parte il gioco
-    btnPlay.addEventListener('click', function(){})
+    btnPlay.addEventListener('click', function(){
         for (let i = 0; i < numeroCelle; i++) {
             let cell = getSquareElement()
             cell.innerHTML = i +1
-            griglia.append(cell)
+            griglia.append(cell);
         }
         
     
 
         function getSquareElement () {
-        let square = document.createElement('div');
-        square.classList.add('cella')
+        const square = document.createElement('div');
+        square.classList.add('cella');
+        square.addEventListener('click', clickCell);
 
         return square
+        
         }
 
 
+        function clickCell() {
+            const square = this
+            square.classList.toggle('clicked');
+            console.log(square);
+        }
 
+})
 
 
 
